2f8ab3404488cad93de60c06d41231b4c9458662
Gaurab Tandukar <gaurabtandukar20@gmail.com>
2026-07-27 13:52:05 +0545

    Profile route configured

--- Vibe-backend-api/routes/userRoute.js
+++ Vibe-backend-api/routes/userRoute.js
@@ -7,6 +7,8 @@ const {
   getUserProfile,
   getAllUsers,
   getUserByUsername,
+  updateUserProfile,
+  updateUserPassword,
 } = require("../controllers/userController");
 const { protect, authorize } = require("../middleware/authMiddleware");
 
@@ -17,6 +19,9 @@ router.post("/login", loginUser);
 // Protected routes
 router.get("/profile", protect, getUserProfile);
 router.get("/profile/:username", protect, getUserByUsername);
+router.get("/all", protect, getAllUsers);
+router.put("/profile/:username", protect, updateUserProfile);
+router.put("/profile/password/:username", protect, updateUserPassword);
 
 // Admin only route
 router.get("/all", protect, authorize("ADMIN"), getAllUsers);
