d232c4c17ba9fc97589cd4d2d7d6707614e80d91
Gaurab Tandukar <gaurabtandukar20@gmail.com>
2026-08-14 22:52:12 +0545

    guest route configured

--- Vibe-frontend/src/routes/AppRoutes.jsx
+++ Vibe-frontend/src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "../components/ProtectedRoute";
+import GuestRoute from "../components/GuestRoute";
 import HomePage from "../pages/home/HomePage";
 import LoginPage from "../pages/login/LoginPage";
 import RegisterPage from "../pages/register/RegisterPage";
@@ -10,18 +11,19 @@ import ContactPage from "../pages/contact/ContactPage";
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route element={<GuestRoute />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+
       <Route path="/about" element={<AboutPage />} />
       <Route path="/contact" element={<ContactPage />} />
-
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
 
       <Route element={<ProtectedRoute />}>
         <Route path="/profile" element={<ProfilePage />} />
       </Route>
 
-      {/* Catch-all: any unknown URL goes home instead of a blank page */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
