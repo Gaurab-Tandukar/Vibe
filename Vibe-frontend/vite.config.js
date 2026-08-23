7c733095ced3047b037f9cf445030f3cf349ba93
Gaurab Tandukar <gaurabtandukar20@gmail.com>
2026-08-23 22:23:13 +0545

    Disable inlining of assets

--- Vibe-frontend/vite.config.js
+++ Vibe-frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    assetsInlineLimit: 0, // Disable inlining of assets to ensure all assets are emitted as separate files
+  },
+});
